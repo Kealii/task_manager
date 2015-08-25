f0aca30b81797187502d62d60843e87eead84475
Michael Wong <michael.brian.wong@gmail.com>
2015-08-25 14:27:05 -0600

    Added all Crud Functionality
    Update and Delete

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -3,6 +3,7 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -27,8 +28,18 @@ class TaskManagerApp < Sinatra::Base
     erb :show
   end
 
-  get '/tasks:id/edit' do |id|
+  get '/tasks/:id/edit' do |id|
     @task = TaskManager.find(id.to_i)
     erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
   end
 end
